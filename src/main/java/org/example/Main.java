85b214a1cbd9df311d168bf54e966ba60361c9c4
Sergey Gerasimov <czar997@yandex.ru>
2023-11-19 17:11:27 +0300

    добавлены тестовые сценарии

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,18 +5,41 @@ import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
+        //Сценарий при котором один пропущенный элемент
         int[] arr1 = {1, 2, 3, 5};
-        int[] arr2 = {5, 0, 1, 3, 2};
-        int[] arr3 = {7, 9, 10, 11, 12};
         System.out.println(findFirstMissingNumber(arr1));
+
+        //Сценарий при котором один пропущенный элемент
+        int[] arr2 = {5, 0, 1, 3, 2};
         System.out.println(findFirstMissingNumber(arr2));
+
+        //Сценарий при котором один пропущенный элемент
+        int[] arr3 = {7, 9, 10, 11, 12};
         System.out.println(findFirstMissingNumber(arr3));
+
+        //Сценарий при котором нет пропущенных элементов
+        int[] arr4 = {0, 1};
+        System.out.println(findFirstMissingNumber(arr4));
+
+        //Сценарий при котором все элементы одинаковые
+        int[] arr5 = {0, 0};
+        System.out.println(findFirstMissingNumber(arr5));
+
+        //Сценарий при котором 2 пропущенных элемента
+        int[] arr6 = {5, 2, 1, 0};
+        System.out.println(findFirstMissingNumber(arr6));
     }
 
     public static int findFirstMissingNumber(int[] arr) {
 
+        // Если пустой или массив с одним элементом
+        if (arr.length <= 1) {
+            return -1;
+        }
+
+        // Находим минимальный элемент. Это будет у нас началом последовательности
         int min = Integer.MAX_VALUE;
-        Set<Integer> set = new HashSet<>();
+        Set<Integer> set = new HashSet<>(); // Создаем множество, для того делать быстрый поиск на отсутствующие элементы
         for (int j : arr) {
             if (j < min) {
                 min = j;
@@ -24,8 +47,10 @@ public class Main {
             set.add(j);
         }
 
+        // Если элемент не найден, то будет возвращаться -1
         int missingNumber = -1;
-        for (int i = min; i <= min + arr.length; i++) {
+        for (int i = min; i < min + set.size(); i++) {
+            // Если элемента нет в множестве, то возвращаем его и выходим из цикла
             if (!set.contains(i)) {
                 missingNumber = i;
                 break;
